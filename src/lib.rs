739f4b9d23b0a35c89549dcc5c2a5c6dc478fcf6
Federico Dionisi <code@fdionisi.me>
2024-10-23 10:46:12 +0200

    feat: use HttpClient

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,24 @@
 pub mod error;
 pub mod search;
 
+use std::sync::Arc;
+
 use anyhow::{anyhow, Result};
 use error::{HttpError, HttpErrorPayload, TavilyError};
+use http_client::{http::Method, HttpClient, Request, RequestBuilderExt, ResponseAsyncBodyExt};
 use secrecy::{ExposeSecret, SecretString};
 use serde::{de::DeserializeOwned, Serialize};
 
 pub const BASE_URL: &str = "https://api.tavily.com";
 
 pub struct Tavily {
-    client: reqwest::Client,
+    http_client: Arc<dyn HttpClient>,
     api_key: SecretString,
     base_url: String,
 }
 
 pub struct TavilyBuilder {
+    http_client: Option<Arc<dyn HttpClient>>,
     api_key: Option<SecretString>,
     base_url: Option<String>,
 }
@@ -29,6 +33,7 @@ struct RequestWithApiKey<'a> {
 impl Tavily {
     pub fn builder() -> TavilyBuilder {
         TavilyBuilder {
+            http_client: None,
             api_key: None,
             base_url: None,
         }
@@ -41,13 +46,16 @@ impl Tavily {
         D: DeserializeOwned,
     {
         let response = self
-            .client
-            .post(format!("{}{}", self.base_url, path.into()))
-            .json(&RequestWithApiKey {
-                api_key: self.api_key.expose_secret(),
-                payload: serde_json::to_value(request).expect("couldn't serialize request"),
-            })
-            .send()
+            .http_client
+            .send(
+                Request::builder()
+                    .uri(format!("{}{}", self.base_url, path.into()))
+                    .method(Method::POST)
+                    .json(&RequestWithApiKey {
+                        api_key: &self.api_key.expose_secret(),
+                        payload: serde_json::to_value(request).expect("couldn't serialize request"),
+                    })?,
+            )
             .await?;
 
         let status = response.status();
@@ -65,19 +73,30 @@ impl Tavily {
 }
 
 impl TavilyBuilder {
-    pub fn api_key(mut self, api_key: String) -> Self {
-        self.api_key = Some(api_key.into());
+    pub fn with_http_client(mut self, http_client: Arc<dyn HttpClient>) -> Self {
+        self.http_client = Some(http_client);
         self
     }
 
-    pub fn base_url(mut self, base_url: String) -> Self {
-        self.base_url = Some(base_url);
+    pub fn with_api_key<S>(mut self, api_key: S) -> Self
+    where
+        S: AsRef<str>,
+    {
+        self.api_key = Some(api_key.as_ref().to_string().into());
+        self
+    }
+
+    pub fn with_base_url<S>(mut self, base_url: S) -> Self
+    where
+        S: AsRef<str>,
+    {
+        self.base_url = Some(base_url.as_ref().into());
         self
     }
 
     pub fn build(self) -> Result<Tavily> {
         Ok(Tavily {
-            client: reqwest::Client::new(),
+            http_client: self.http_client.ok_or_else(|| anyhow!("http_client must be specified"))?,
             api_key: self.api_key.or_else(|| std::env::var("TAVILY_API_KEY").ok().map(SecretString::new))
                 .ok_or_else(|| anyhow!("API key is required. Set it explicitly or use the TAVILY_API_KEY environment variable"))?,
             base_url: self.base_url.unwrap_or_else(|| BASE_URL.to_string()),
